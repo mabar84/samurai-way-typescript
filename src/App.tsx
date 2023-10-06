180b487783a60172e508cec0342da8a7ce6c7e86
mabar84 <mabar@tut.by>
2023-10-06 12:06:10 +0300

    Samurai-way 8 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
 
 export const App=()=> {
   return (
     <div className="App">
         <Header/>
-     Hello, samurai! Let's go!
+         Hello, samurai! Let's go!
     </div>
   );
-}
-
-const Header = ()=>{
-    return <>
-        <ul>
-            <li><a href="#"> Home </a></li>
-            <li><a href="#"> Works </a></li>
-            <li><a href="#"> Contacts </a></li>
-        </ul>
-    </>
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = ()=>{
+    return <>
+        <ul>
+            <li><a href="#"> Home </a></li>
+            <li><a href="#"> Works </a></li>
+            <li><a href="#"> Contacts </a></li>
+        </ul>
+    </>
+}
