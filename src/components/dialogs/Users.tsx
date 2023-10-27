7986b38f8fb185b7b1d83e88f57bd993433474f5
mabar84 <mabar@tut.by>
2023-10-27 14:58:44 +0300

    Users refacroring, map added

--- src/components/dialogs/Users.tsx
+++ src/components/dialogs/Users.tsx
@@ -3,13 +3,26 @@ import {styled} from 'styled-components';
 import {User} from './User';
 
 export const Users = () => {
+
+    const users = [
+        {
+            id: 1,
+            name: 'Petr'
+        }, {
+            id: 2,
+            name: 'Aramis'
+        }, {
+            id: 3,
+            name: 'Guffi'
+        }, {
+            id: 4,
+            name: 'Donatello'
+        },
+    ]
+
     return (
         <StyledUsers>
-
-            <User name={'Petr'} id={1}/>
-            <User name={'Aramis'} id={2}/>
-            <User name={'Guffi'} id={3}/>
-            <User name={'Donatello'} id={4}/>
+            {users.map(el => <User id={el.id} name={el.name}/>)}
         </StyledUsers>
     );
 };
