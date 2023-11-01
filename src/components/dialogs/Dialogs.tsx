796a2f63b0ef798b287a1b9e65551f650b48b7eb
mabar84 <mabar@tut.by>
2023-11-01 22:44:22 +0300

    addPost to Dialogs is done. les31 is done

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,6 +3,7 @@ import {styled} from 'styled-components';
 import {Users} from './Users';
 import {Messages} from './Messages';
 import {MessageType, UserType} from '../../state/state';
+import {AddPost} from '../addPost/AddPost';
 
 type DialogsPropsType = {
     messagesData: MessageType[]
@@ -12,15 +13,37 @@ type DialogsPropsType = {
 export const Dialogs: React.FC<DialogsPropsType> = (props) => {
     return (
         <StyledDialogs>
-            <Users usersData={props.usersData}/>
+            <div className="grid">
+                <Users usersData={props.usersData}/>
 
-            <Messages messagesData={props.messagesData}/>
+                <Messages messagesData={props.messagesData}/>
+            </div>
+
+
+            <AddPost/>
         </StyledDialogs>
     );
 };
 
 const StyledDialogs = styled.div`
-  display: grid;
-  grid-template-columns: minmax(100px, max-content) 1fr;
-  gap: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: space-between;
+  height: 100%;
+
+  .grid {
+    display: grid;
+    grid-template-columns: minmax(100px, max-content) 1fr;
+    gap: 30px;
+  }
+
+  .addPost {
+    width: 100%;
+  }
+
+
+
+
+
 `
