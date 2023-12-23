b5f770bf0ef8763db1b52c086eab0c5b85e00696
mabar84 <mabar@tut.by>
2023-12-23 23:38:54 +0300

    messages-reducer.test.ts added

---
+++ src/store/messages-reducer.test.ts
@@ -0,0 +1,31 @@
+import {AddMessageAC, AddPostAC} from './store';
+import {messagesReducer} from './messages-reducer';
+
+test('new post text should be added', () => {
+    const startState = {
+        currentTextareaValue: 'Some new message',
+        messagesData: [
+            {
+                id: 1,
+                message: 'Hi! I\'m a window specialist with great experience.',
+                isMine: false
+            },
+            {
+                id: 2,
+                message: 'I don\'t need to fix my window',
+                isMine: true
+            },
+            {
+                id: 3,
+                message: 'Do you need a barber?',
+                isMine: false
+            },
+        ],
+    }
+    const endState = messagesReducer(startState, AddMessageAC())
+
+    expect(endState.currentTextareaValue).toBe('')
+    expect(endState.messagesData.length).toBe(4)
+    expect(endState.messagesData[3].message).toBe('Some new message')
+    expect(endState.messagesData[3].id).toBe(4)
+})
