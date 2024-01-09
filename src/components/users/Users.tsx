d65416b0a05cbd87ce529f2e64043b028b8821e7
mabar84 <mabar@tut.by>
2024-01-09 15:23:30 +0300

    button getUsers added les52 is done

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -9,14 +9,22 @@ export const Users = () => {
     let users = useSelector<AppStateType, UserType[]>(state => state.usersData)
     const dispatch = useDispatch()
 
-    useEffect(() => {
+    const getUsers = () => {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => {
                 dispatch(SetUsersAC(res.data.items))
             })
-    }, [])
+    }
+
+    // useEffect(() => {
+    //     axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    //         .then(res => {
+    //             dispatch(SetUsersAC(res.data.items))
+    //         })
+    // }, [])
 
     return <>
+        <button onClick={getUsers}>getUsers</button>
         {users.map(u => <User key={u.id} user={u}/>)}
     </>
 }
