dcb54a96c1fe6e4f8b62b550a74053242d1f5a4e
mabar84 <mabar@tut.by>
2024-01-12 14:12:18 +0300

    life cycle methods les54 is done

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
 import {UserType} from '../../store/store';
 import {User} from './User';
 import axios from 'axios';
@@ -10,39 +9,16 @@ type UsersPropsType = {
 }
 
 export class Users extends React.Component<UsersPropsType> {
-    // let users = useSelector<AppStateType, UserType[]>(state => state.usersData)
-    // const dispatch = useDispatch()
-
-    constructor(props: UsersPropsType) {
-        super(props);
-        alert('NEW')
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => {
                 this.props.setUsers(res.data.items)
-                // dispatch(SetUsersAC(res.data.items))
             })
     }
 
-    // getUsers = () => {
-    //     axios.get('https://social-network.samuraijs.com/api/1.0/users')
-    //         .then(res => {
-    //             this.props.setUsers(res.data.items)
-    //             // dispatch(SetUsersAC(res.data.items))
-    //         })
-    // }
-
     render() {
         return <>
-            {/*<button onClick={this.getUsers}>getUsers</button>*/}
             {this.props.usersData.map(u => <User key={u.id} user={u}/>)}
         </>
     }
 }
-
-// useEffect(() => {
-//     axios.get('https://social-network.samuraijs.com/api/1.0/users')
-//         .then(res => {
-//             dispatch(SetUsersAC(res.data.items))
-//         })
-// }, [])
-
